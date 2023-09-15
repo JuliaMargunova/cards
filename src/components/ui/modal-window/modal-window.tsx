cf47b0e62f9248d28ba5ced113dc99c9fb12c5a6
Aleksei <bestuzhev.aleksei.m@gmail.com>
2023-09-15 22:31:10 +0400

    upd: add ModalWindow

---
+++ src/components/ui/modal-window/modal-window.tsx
@@ -0,0 +1,45 @@
+import { FC, PropsWithChildren, ReactNode } from 'react'
+
+import * as Dialog from '@radix-ui/react-dialog'
+
+import s from './modal-window.module.scss'
+
+import { Card } from '@/components/ui/card'
+import { Icon } from '@/components/ui/icon/icon.tsx'
+import { IconButton } from '@/components/ui/icon-button'
+import { Typography } from '@/components/ui/typography'
+
+type Props = {
+  open: boolean
+  setOpen: (value: boolean) => void
+  trigger?: ReactNode
+  title: string
+} & PropsWithChildren
+
+export const ModalWindow: FC<Props> = ({ open, setOpen, title, children }) => {
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Portal>
+        <Dialog.Overlay className={s.overlay} />
+        <div className={s.root}>
+          <Dialog.Content>
+            <Card className={s.card}>
+              <div className={s.header}>
+                <Typography variant="h2" as="h2" className="DialogTitle">
+                  {title}
+                </Typography>
+                <Dialog.Close asChild>
+                  <IconButton
+                    icon={<Icon name={'cross'} width={22} height={22} />}
+                    aria-label="Close"
+                  />
+                </Dialog.Close>
+              </div>
+              <div className={s.content}>{children}</div>
+            </Card>
+          </Dialog.Content>
+        </div>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
